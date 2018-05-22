d6da282df7f2bbc6eced5b21bb5cd3c74ac2d41e
sai sharan <s.sharan2008@gmail.com>
2018-05-21 21:25:29 -0400

    changes

--- src/app/video-detail/video-detail.component.ts
+++ src/app/video-detail/video-detail.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit , OnDestroy} from '@angular/core';
 import  {ActivatedRoute} from '@angular/router';
+// provider taht allows us  workwith route that  has been suggested to us and getting params fro that route.
+
+
 
 
 @Component({
@@ -7,14 +10,19 @@ import  {ActivatedRoute} from '@angular/router';
   templateUrl: './video-detail.component.html',
   styleUrls: ['./video-detail.component.css']
 })
-export class VideoDetailComponent implements OnInit {
+export class VideoDetailComponent implements OnInit, OnDestroy {
   private routeSub:any;
+  slug : string;
   constructor(private route:ActivatedRoute) { }
 
   ngOnInit() {
-    this.routeSub = this.route.params.subscribe(Params =>{
-      console.log(Params);
+    this.routeSub = this.route.params.subscribe(params =>{
+      //console.log(Params);
+      this.slug = params['slug'];
     })
+  }
+  ngOnDestroy(){
+  this.routeSub.unSubscribe()
   }
 
 }
